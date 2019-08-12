9f6c2b0abb9e5ffe1e5068565cf37d98c0183c2c
Micah Levine <micah.levine@outlook.com>
2019-08-12 19:40:35 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-add_element_to_end_of_array << "Puumba"
+create_an_array << "Puumba"
 end
 
 def add_element_to_start_of_array(array, element)
